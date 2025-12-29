492cb050a3c7ba306ca266c6be24db78fc8afbde
Avishek <avishekprasad12345@gmail.com>
2025-12-29 13:08:29 +0530

    Static and Extension

--- Day9/Indexer/Program.cs
+++ Day9/Indexer/Program.cs
@@ -5,6 +5,7 @@ namespace IndexerExample
     {
         public static void Main(string[] args)
             {
+                #region Indexer
                 // MyData a=new MyData();
                 // a[0]="C";//a[0] means first item of that particular object and so on..
                 // a[1]="C++";
@@ -13,20 +14,31 @@ namespace IndexerExample
                 // System.Console.WriteLine($"The first value of the indexer is {a[0]}");
                 // System.Console.WriteLine($"The second value of the indexer is {a[1]}");
                 // System.Console.WriteLine($"The third value of the indexer is {a[2]}");
+                #endregion
 
-                Student s1=new Student();
-                s1.RollNo=1;
-                s1.Name="Avishek";
-                s1.Address="Punjab";
-                s1[0]="Mahabharat";
-                s1[1]="Ramayan";
+                #region IndexerPractice
+                // //New object of class Student and then initialization
+                // Student s1=new Student();
+                // s1.RollNo=1;
+                // s1.Name="Avishek";
+                // s1.Address="Punjab";
+                // s1[0]="Mahabharat";//initializing values for the first index
+                // s1[1]="Ramayan";//initializing values for the second index
+                
 
-                System.Console.WriteLine($"The name of the student is {s1.Name}");
-                System.Console.WriteLine($"The Roll No of the student is {s1.RollNo}");
-                System.Console.WriteLine($"The Address of the student is {s1.Address}");
-                System.Console.WriteLine($"The first book of the student is {s1[0]}");
-                System.Console.WriteLine($"The second book of the student is {s1[1]}");
 
+                // //Printing to check output 
+                // System.Console.WriteLine($"The name of the student is {s1.Name}");
+                // System.Console.WriteLine($"The Roll No of the student is {s1.RollNo}");
+                // System.Console.WriteLine($"The Address of the student is {s1.Address}");
+                // System.Console.WriteLine($"The first book of the student is {s1[0]}");
+                // System.Console.WriteLine($"The second book of the student is {s1[1]}");
+                #endregion
+
+                #region  PrivateSet
+                Console.WriteLine("Hello, World!");
+                YoungProfessional yp = new YoungProfessional();
+                #endregion
             }
     }
 }
--- Day9/Indexer/StudentIndexer.cs
+++ Day9/Indexer/StudentIndexer.cs
@@ -1,8 +1,11 @@
 class Student
 {
-    public int RollNo{get;set;}
-    public string Name{get;set;}
-    private string _Address;
+    public int RollNo{get;set;}//public variable for Roll No
+    public string Name{get;set;}//public variable for Name
+    private string _Address;//priavte filed for Address so others will not access it and make changes
+    /// <summary>
+    /// Property for the field _Address which sets and gets its value
+    /// </summary>
     public string Address
     {
         set
@@ -15,10 +18,16 @@ class Student
         }
         get
         {
-            return string.IsNullOrEmpty(this._address)?"No Value":this._address;
+            return string.IsNullOrEmpty(this._Address)?"No Value":this._Address;
         }
     }
-    private string[] books=new string[2];
+    // private string[] books=new string[2];//private indexer for storing book values in string format <- array type fixed though
+    private List<String> books=new List<string>();//list 
+    /// <summary>
+    /// Get and set accessors  for private field book .
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
     public string this[int index]
     {
         get
@@ -27,6 +36,10 @@ class Student
         }
         set
         {
+            while (index >= books.Count)
+            {
+                books.Add("");
+            }
             books[index]=value;
         }
     }
