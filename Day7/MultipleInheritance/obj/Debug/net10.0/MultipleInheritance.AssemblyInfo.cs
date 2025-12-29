0a5bd43a048199165c1dd60e93f7ea668ec3cd29
Avishek <avishekprasad12345@gmail.com>
2025-12-29 10:16:17 +0530

    Indexers Example

--- Day7/MultipleInheritance/obj/Debug/net10.0/MultipleInheritance.AssemblyInfo.cs
+++ Day7/MultipleInheritance/obj/Debug/net10.0/MultipleInheritance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultipleInheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71eb94172dee06ed0ee16f18cf8a1826feff4b21")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultipleInheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultipleInheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Day9/Indexer/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace IndexerExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            {
+                MyData a=new MyData();
+                a[0]="C";//a[0] means first item of that particular object and so on..
+                a[1]="C++";
+                a[2]="C#";
+
+                System.Console.WriteLine($"The first value of the indexer is {a[0]}");
+                System.Console.WriteLine($"The second value of the indexer is {a[1]}");
+                System.Console.WriteLine($"The third value of the indexer is {a[2]}");
+
+            }
+    }
+}
