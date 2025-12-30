68f0cc20504711ed89cbd357825e3fa52d4903a3
Avishek <avishekprasad12345@gmail.com>
2025-12-30 14:18:53 +0530

    Garbage Collecter and Using IDisposeable

--- Day-10-ExtenAndRegEx/Extensions/RegEx.cs
+++ Day-10-ExtenAndRegEx/Extensions/RegEx.cs
@@ -6,14 +6,15 @@ namespace RegEx
     {
         static void Main(string[] agrs)
         {
-            string input="Error: Timeout from API";
-            string patter=@"timeout";
-            var rx=new Regex
-            (
-                patter,RegexOptions.IgnoreCase,
-                TimeSpan.FromMilliseconds(1)//The time written is the limit if cant run within this limit then we get error
-            );
-            System.Console.WriteLine(rx.IsMatch(input)?"Found":"Not Found");
+            // string input="Error: Timeout from API";
+            // string patter=@"timeout";
+            // var rx=new Regex
+            // (
+            //     patter,RegexOptions.IgnoreCase,
+            //     TimeSpan.FromMilliseconds(1)//The time written is the limit if cant run within this limit then we get error
+            // );
+            // System.Console.WriteLine(rx.IsMatch(input)?"Found":"Not Found");
+            
     }
 }
 }
