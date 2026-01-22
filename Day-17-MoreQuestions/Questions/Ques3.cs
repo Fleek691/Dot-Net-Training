4cd732ecfdc5a6ca8e454f1f7a5454cd076ca620
Avishek <avishekprasad12345@gmail.com>
2026-01-22 09:55:23 +0530

    Attribute Usage

--- Day-17-MoreQuestions/Questions/Ques3.cs
+++ Day-17-MoreQuestions/Questions/Ques3.cs
@@ -1,28 +1,47 @@
-// public interface IBook
-// {
-//     int Id{get;set;}
-//     string? Title{get;set;}
-//     string? Author{get;set;}
-//     string? Category{get;set;}
-//     int Price{get;set;}
-// }
-// public interface ILibrarySystem
-// {
-//     Dictionary<IBook,int> _books{get;set;}
-//     void AddBook(IBook book,int quantity){}
-//     void RemoveBook(IBook book,int quantity){}
-//     int CalculateTotal(){return 0;}
+public interface IBook
+{
+    int Id{get;set;}
+    string? Title{get;set;}
+    string? Author{get;set;}
+    string? Category{get;set;}
+    int Price{get;set;}
+}
+public interface ILibrarySystem
+{
+    private Dictionary<IBook,int> _books;
+    void AddBook(IBook book,int quantity){}
+    void RemoveBook(IBook book,int quantity){}
+    int CalculateTotal(){return 0;}
+    List<(string,int)> CategoryTotalPrice();
+    List<(string,int)> BooksInfo();
+    List<(string,int)> CategoryAndAuthorWithCount();
     
-// }
-// public class Book : IBook
-// {
-//     public int Id { get ; set ; }
-//     public string? Title { get ; set ; }
-//     public string? Author { get ; set ; }
-//     public string? Category { get ; set ; }
-//     public int Price { get ; set ; }
-// }
-// public class LibrarySystem: ILibrarySystem
-// {
-    
-// }
+}
+public class Book : IBook
+{
+    public int Id { get ; set ; }
+    public string? Title { get ; set ; }
+    public string? Author { get ; set ; }
+    public string? Category { get ; set ; }
+    public int Price { get ; set ; }
+}
+public class LibrarySystem : ILibrarySystem
+{
+    public Dictionary<IBook, int> _books { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    Dictionary<IBook, int> ILibrarySystem._books { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    List<(string, int)> ILibrarySystem.BooksInfo()
+    {
+        throw new NotImplementedException();
+    }
+
+    List<(string, int)> ILibrarySystem.CategoryAndAuthorWithCount()
+    {
+        throw new NotImplementedException();
+    }
+
+    List<(string, int)> ILibrarySystem.CategoryTotalPrice()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Day-18-Attributes/Att/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Att
+{
+    [Obsolete("Use the Add method this is old")]
+    public static  int OldAdd(int a,int b)
+    {
+        return a+b;
+    }
+    public static  int Add(int a,int b)
+    {
+        return a+b;
+    }
+
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine(OldAdd(1,2));
+        System.Console.WriteLine(Add(2,3));
+    }
+}
