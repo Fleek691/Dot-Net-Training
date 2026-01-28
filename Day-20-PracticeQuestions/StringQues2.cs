eb3bc6781dc80535256056d52c8984857b278e33
Avishek <avishekprasad12345@gmail.com>
2026-01-28 14:14:05 +0530

    Some more practice

---
+++ Day-20-PracticeQuestions/StringQues2.cs
@@ -0,0 +1,15 @@
+public class Ques2
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter the Man Name");
+        string man=Console.ReadLine()!;
+        System.Console.WriteLine("Enter the Woman Name");
+        string Woman=Console.ReadLine()!;
+
+    }
+    public bool IsEligible()
+    {
+        return true;
+    }
+}
