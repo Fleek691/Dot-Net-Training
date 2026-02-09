6d21bbfb2f638cf1c99c130a67fb5f6c530735d2
Avishek <avishekprasad12345@gmail.com>
2026-02-09 23:48:04 +0530

    Notification Ques

--- Day-26.1-M1QuesPrac/Notification/Program.cs
+++ Day-26.1-M1QuesPrac/Notification/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public interface INotifier
+{
+    public void Send(string Message);
+}
+public class Email : INotifier
+{
+    public void Send(string Message)
+    {
+        System.Console.WriteLine($"Email Notification :{Message}");
+    }
+}
+public class WhatsApp : INotifier
+{
+    public void Send(string Message)
+    {
+        System.Console.WriteLine($"WhatsApp Notification :{Message}");
+    }
+}
+public class Sms : INotifier
+{
+    public void Send(string Message)
+    {
+        System.Console.WriteLine($"Sms Notification :{Message}");
+    }
+}
+public class Helper
+{
+    public static void Main()
+    {
+        List<INotifier> notification=new List<INotifier>();
+        WhatsApp wp=new WhatsApp();
+        Email em=new();
+        Sms sms=new();
+        notification.Add(wp);
+        notification.Add(em);
+        notification.Add(sms);
+        System.Console.Write("Enter Message: ");
+        string Message=Console.ReadLine()!;
+        foreach (var item in notification)
+        {
+            item.Send(Message);
+        }
+    }
+}
