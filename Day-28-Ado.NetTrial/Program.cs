8a2e49185689e425575149cf948a1d3e02d29958
Avishek <avishekprasad12345@gmail.com>
2026-02-13 15:02:02 +0530

    Basics of Ado.Net

---
+++ Day-28-Ado.NetTrial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string cs =
+        "Data Source=FLEEK\\SQLEXPRESS;Initial Catalog=Customer-Order;Integrated Security=True;Encrypt=False;";
+
+        string sql = "SELECT CustomerId, FullName, City FROM dbo.Customers";
+
+        using SqlConnection con = new SqlConnection(cs);
+        using SqlCommand cmd = new SqlCommand(sql, con);
+
+        con.Open();
+
+        using SqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine(
+                $"{reader["CustomerId"]} | " +
+                $"{reader["FullName"]} | " +
+                $"{reader["City"]}"
+            );
+        }
+    }
+}
