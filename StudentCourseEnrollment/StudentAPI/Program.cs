e84f94f4255f124ebf46e135c0c4dca6ddfc98da
Avishek <avishekprasad12345@gmail.com>
2026-03-19 22:57:05 +0530

    Update

--- StudentCourseEnrollment/StudentAPI/Program.cs
+++ StudentCourseEnrollment/StudentAPI/Program.cs
@@ -29,3 +29,37 @@ app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
 
+// cd C:\CIT
+// .\.venv\Scripts\activate
+// python -m pip install jupyterlab
+// jupyter lab	
+
+
+// *********************************************
+// var builder = WebApplication.Create();
+// var app = builder.Build();
+
+// // A simple Fluent chain for a GET request
+// app.MapGet("/books/{id}", (int id) => 
+// {
+//     return Results.Ok(new { Id = id, Title = "The Great Gatsby" });
+// })
+// .WithName("GetBookById")
+// .WithSummary("Retrieves a single book by its unique ID");
+
+// app.Run();
+
+
+
+// ___________________________
+
+
+// // A Fluent chain for a POST request
+// app.MapPost("/books", (Book newBook) => 
+// {
+//     // Logic to save the book to a database would go here
+//     return Results.Created($"/books/{newBook.Id}", newBook);
+// })
+// .WithName("CreateBook")
+// .Accepts<Book>("application/json")
+// .Produces<Book>(201);
--- WebApplicationTrial/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationTrial.Global" Language="C#" %>
